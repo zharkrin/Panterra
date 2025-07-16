c03cc1dfb42aab6e049ad9dccbec46617fc743c1
zharkrin <zarkrin2@gmail.com>
2025-07-16 15:53:03 +0200

    Actualizar run.py

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, send_from_directory
+import os
 
 app = Flask(__name__, static_folder='frontend/assets', template_folder='frontend')
 
@@ -13,6 +14,14 @@ def send_js(path):
 @app.route('/assets/<path:path>')
 def send_assets(path):
     return send_from_directory('frontend/assets', path)
+
+@app.route('/naciones/<id>')
+def submapa_nacion(id):
+    ruta_archivo = f'frontend/naciones/{id}.html'
+    if os.path.exists(ruta_archivo):
+        return render_template(f'naciones/{id}.html')
+    else:
+        return render_template('naciones/base.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
