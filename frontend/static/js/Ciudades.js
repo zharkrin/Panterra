28a72e9df9e4c9c935edbdf193405f8946db5560
zharkrin <zarkrin2@gmail.com>
2025-08-04 13:19:26 +0200

    Create Ciudades.js

---
+++ frontend/static/js/Ciudades.js
@@ -0,0 +1,44 @@
+// frontend/static/js/ciudades.js
+
+export const ciudades = [
+  {
+    id: 1,
+    nombre: "Ciudad de las Sombras",
+    nivel: 1,
+    x: 5,
+    y: 6,
+    tipo: "ciudad"
+  },
+  {
+    id: 2,
+    nombre: "Pueblo Hongos Vivos",
+    nivel: 1,
+    x: 8,
+    y: 3,
+    tipo: "pueblo"
+  },
+  {
+    id: 3,
+    nombre: "Fortaleza del Crisol",
+    nivel: 2,
+    x: 10,
+    y: 12,
+    tipo: "ciudad"
+  },
+  {
+    id: 4,
+    nombre: "Campamento Ruinas Viejas",
+    nivel: 2,
+    x: 3,
+    y: 8,
+    tipo: "pueblo"
+  },
+  {
+    id: 5,
+    nombre: "Mazmorra Abisal",
+    nivel: 3,
+    x: 7,
+    y: 5,
+    tipo: "mazmorra"
+  }
+];
